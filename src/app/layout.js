bfbac5304892bc52b841380c9c9d0e2f1ef8a39c
Hasankhankor <hkhankor@gmail.com>
2024-02-14 13:17:38 +0500

    changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,14 +16,14 @@ export default function RootLayout({ children }) {
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
           rel="stylesheet"
           integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         ></link>
       </head>
       <body className={inter.className}>{children}</body>
       <script
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossorigin="anonymous"
+        crossOrigin="anonymous"
       ></script>
       
     </html>
